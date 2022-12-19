6c4553de2ddadb88583e2994adaa7b61a4e01e32
marzie79 <marzie.7900@gmail.com>
2022-12-19 14:22:04 +0330

    add .gitignore and rename the project

--- django_replication_postgres/asgi.py
+++ django_replication_postgres/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_cqrs project.
+ASGI config for django_replication_postgres project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_cqrs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'django_replication_postgres.settings')
 
 application = get_asgi_application()
--- django_replication_postgres/wsgi.py
+++ django_replication_postgres/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_cqrs project.
+WSGI config for django_replication_postgres project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_cqrs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'django_replication_postgres.settings')
 
 application = get_wsgi_application()
