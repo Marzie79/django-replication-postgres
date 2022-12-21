0e74dd39132bb9f3348e5c159dcee3b9b5390c7d
Marzie79 <marzie.7900@gmail.com>
2022-12-21 18:59:51 +0330

    Add automation status to main router

--- django_replication_postgres/main_router.py
+++ django_replication_postgres/main_router.py
@@ -7,27 +7,54 @@ from django.db.utils import OperationalError
 class MainRouter:
     """A router to control read and write database operations on models."""
 
+    def __init__(self) -> None:
+        self.__databases = {'replica1', 'replica2'}
+        self.__available_databases = {'replica1', 'replica2'}
+        self.__default_database = "default"
+
     @staticmethod
-    def connection_status(database='default'):
-        """Try to find a database that is available."""
-        status = True
-        db_connection = connections[database]
+    def _connection_status(database='default') -> bool:
+        """Try to find a database that is available.
+
+        Args:
+            database: The database name that we want to test connection.
+
+        Returns:
+            The status of database connection. 
+        """
         try:
-            db_connection.cursor()
+            connections[database].cursor()
+            return True
         except OperationalError:
-            status = False
-        finally:
-            return status
+            return False
+
+    def _select_database(self) -> str:
+        """Select a random database from available databases.
+
+        Returns:
+            The selected database.
+        """
+        if not self.__available_databases:
+            return self.__default_database
+        return random.choice(list(self.__available_databases))
+
+    def _reset_availabe_databases(self):
+        """Reset available database to its first condition."""
+        self.__available_databases = self.__databases.copy()
 
     def db_for_read(self, model, **hints):
-        """Point all operations randomly on all models to replica1 or replica2."""
-        database = random.choice(['replica1', 'replica2'])
-
-        if self.connection_status(database=database):
+        """Point all operations on all models to an available database."""
+        database = self._select_database()
+        if self._connection_status(database=database):
+            self._reset_availabe_databases()
             return database
-        else:
+        elif database != self.__default_database:
+            self.__available_databases.remove(database)
             self.db_for_read(self)
+        else:
+            self._reset_availabe_databases()
+            raise ConnectionError()
 
     def db_for_write(self, model, **hints):
         """Point all operations on all models to default."""
-        return "default"
+        return self.__available_databases
